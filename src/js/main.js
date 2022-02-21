ad5ed7da2410575e5bab2598cb82de1887a3584b
Lakshmikant K <lakshmikant2512@gmail.com>
2022-02-21 17:20:48 +0530

    add game type selection styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,31 @@
 console.log("%cTIC TAC TOE", "font-size:1.5rem;color:#fad;font-weight:bold");
+
+const gameTypeDivs = document.querySelectorAll(".game-type");
+
+gameTypeDivs.forEach((gameTypeDiv) => {
+  gameTypeDiv.addEventListener("click", () => {
+    gameTypeDiv.blur();
+    clearSelection();
+    addTypeSelection(gameTypeDiv);
+  });
+  gameTypeDiv.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+      gameTypeDiv.blur();
+      clearSelection();
+      addTypeSelection(gameTypeDiv);
+    }
+  });
+});
+
+function clearSelection() {
+  gameTypeDivs.forEach((div) => {
+    div.classList.remove("add-border");
+  });
+}
+
+function addTypeSelection(type) {
+  type.classList.add("add-animation", "add-border");
+  setTimeout(() => {
+    type.classList.remove("add-animation");
+  }, 750);
+}
